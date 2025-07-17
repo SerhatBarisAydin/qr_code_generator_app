8de64d81e7b63977322486bd4d2a6e753e4f2122
Serhat Barış AYDİN <serhatbarisaydin@gmail.com>
2025-07-17 19:07:11 +0300

    new update

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RootNavigation from "./src/navigation/RootNavigation";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+const App = () => {
+  return (
+    <GestureHandlerRootView>
+        <RootNavigation />
+    </GestureHandlerRootView>
+  );
+};
+export default App;
